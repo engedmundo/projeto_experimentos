7ac0fa9f1c2c3be5c6d6bc85fff357bc633323a0
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-05-11 20:30:03 -0300

    mediana

--- medias/equacoes/mediana.py
+++ medias/equacoes/mediana.py
@@ -6,5 +6,10 @@ def calcula_mediana(dados):
     qtd_dados = len(dados)
     resto_divisao = qtd_dados % 2
     indice = math.floor(qtd_dados / 2)
-    mediana = lista_ordenada[indice]
+
+    if resto_divisao == 0:  # par
+        mediana = (lista_ordenada[indice] + lista_ordenada[indice - 1]) / 2
+    else:  # se for impar
+        mediana = lista_ordenada[indice]
+
     return mediana
