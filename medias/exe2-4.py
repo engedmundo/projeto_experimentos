190be9f3ab4b599ad718cd2b573e6bcc719d5589
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-05-11 20:17:35 -0300

    exe

--- medias/exe2-4.py
+++ medias/exe2-4.py
@@ -1,10 +1,8 @@
 from equacoes.media import calcula_media
 from equacoes.mediana import calcula_mediana
 
-
-# endereço=    0    1    2     3     4     5     6    ....
 lista_dados = [1.7, 2.2, 3.9, 3.11, 14.7]
 media = calcula_media(lista_dados)
-print(f"Média -> {media:.2f}")
+print(media)
 mediana = calcula_mediana(lista_dados)
-print(f"Mediana -> {mediana}")
+print(mediana)
