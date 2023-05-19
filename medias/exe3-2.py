63f03a5f7dce3bd6274562e32b904b379ead5ec4
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-05-18 22:06:04 -0300

    exemplo 3.2

--- medias/exe3-2.py
+++ medias/exe3-2.py
@@ -17,3 +17,14 @@ area_cauda = 0.2676  # Veio da tabela A1
 probabilidade = (1 - area_cauda) * 100
 print(f"Probabilidade de retirar um grão de feijão maior do que {peso}g")
 print(f"é de {probabilidade:.2f}%")
+
+
+# b)
+peso_padronizado_minimo = -1.96  # sai da tabela A1
+peso_padronizado_maximo = 1.96  # sai da tabela A1
+peso_minimo = peso_padronizado_minimo * desv_padrao_pop + media_pop
+peso_maximo = peso_padronizado_maximo * desv_padrao_pop + media_pop
+print(f"Resposta B:")
+print(f"Para uma probabilidade de 95%, os valores do intervalo são:")
+print(f"Peso mínimo: {peso_minimo:.2f}g")
+print(f"Peso máximo: {peso_maximo:.2f}g")
