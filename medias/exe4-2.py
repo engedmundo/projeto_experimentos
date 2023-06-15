58378492ab9cfc10b3018491aa83494d3901dcdb
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-06-15 19:45:14 -0300

    4.2 dados

---
+++ medias/exe4-2.py
@@ -0,0 +1,19 @@
+""""
+Um engenheiro está estudando o efeito de duas diferentes formulações de
+polímero na sua resistência à tração. Cada formulação é um nível de um
+fator ou tratamento. São fabricadas 10 amostras de cada tratamento
+(10 réplicas)
+
+A partir dos dados apresentados da tabela 1, efetue e responda:
+a) Determine a média, variância e desvio padrão;
+b) Há diferença significativa entre as duas amostras? (aplique o teste t)
+"""
+formulacao_01 = [
+    16.85, 16.40, 17.21, 16.35, 16.52,
+    17.04, 16.96, 17.15, 16.59, 16.57,
+]
+
+formulacao_02 = [
+    16.62, 16.75, 17.37, 17.12, 16.98,
+    16.87, 17.34, 17.02, 17.08, 17.27,
+]
