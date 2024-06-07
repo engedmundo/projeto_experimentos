9cb5c6b30ab87dc7c04ba3ac5797342936701091
Edmundo Abreu e Lima <98054216+engedmundo@users.noreply.github.com>
2024-06-07 20:52:34 -0300

    Create tabela_normal.py

---
+++ turma-2024-01/tabela_normal.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import norm
+
+tabela_normal_padronizada = pd.DataFrame(
+    list(),
+    index=["{0:0.2f}".format(i/100) for i in range(0, 400, 10)],
+    columns=["{0:0.2f}".format(i/100) for i in range(0,10)],
+)
+for index in tabela_normal_padronizada.index:
+    for column in tabela_normal_padronizada.columns:
+        value_z = np.round(float(index) + float(column), 2)
+        tabela_normal_padronizada.loc[index, column] = "{0:0.4f}".format(norm.cdf(value_z))
+
+tabela_normal_padronizada.rename_axis("Z",axis="columns", inplace=True)
+tabela_normal_padronizada
